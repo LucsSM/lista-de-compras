8b38d56264b41d0f045946adf80ee7eb52a9ef98
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2023-09-28 18:47:54 -0300

    main.js com propriedades de salvar, mostrar e trocar itens de listas

---
+++ main.js
@@ -0,0 +1,91 @@
+let listaDeItens = [];
+
+const formulario = document.getElementById("form-itens");
+const formularioImput = document.getElementById("receber-item");
+const listaDeCompras = document.getElementById("lista-de-itens");
+const listaItensComprados = document.getElementById("itens-comprados");
+
+formulario.addEventListener("submit", evento => {
+
+    evento.preventDefault();
+    salvarItem();
+    mostrarItem();
+    formularioImput.focus();
+})
+
+function salvarItem() {
+
+    const item = formularioImput.value;
+    const checarDuplicado = listaDeItens.some(elemento => elemento.valor.toUpperCase() === item.toUpperCase())
+
+    if(checarDuplicado) {
+
+        alert("Item já existe");
+    } else {
+
+        listaDeItens.push({
+            valor: item,
+            checar: false
+        })
+    }
+
+    formularioImput.value = "";
+}
+
+function mostrarItem() {
+
+    listaDeCompras.innerHTML = "";
+    listaItensComprados.innerHTML = "";
+    listaDeItens.forEach((elemento, index) => {
+
+        if(elemento.checar) {
+
+            listaItensComprados.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" checked class="is-clickable" data-checkbox/>
+                        <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar" data-deletar></i>
+                    </div>
+                </li>
+            `
+
+        } else {
+
+            listaDeCompras.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" class="is-clickable" data-checkbox/>
+                        <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                    </div>
+                    <div>
+                        <button><i class="fa-regular fa-floppy-disk is-clickable"></i></button>
+                        <i class="fa-regular is-clickable fa-pen-to-square editar" data-editar></i>
+                        <i class="fa-solid fa-trash is-clickable deletar" data-deletar></i>
+                    </div>
+                </li>
+            `;
+        }
+    });
+
+    listarItemComprado();
+}
+
+function listarItemComprado () {
+
+    const itemCheckbox = document.querySelectorAll("[data-checkbox]");
+    
+    itemCheckbox.forEach(i => {
+        
+        i.addEventListener("click", evento => {
+            
+            const itemIndex = evento.target.parentElement.parentElement.getAttribute("data-value");
+            
+            listaDeItens[itemIndex].checar = evento.target.checked;
+            
+            mostrarItem();
+        });
+    });
+}
